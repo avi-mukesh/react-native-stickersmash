74f0969545eab0449c61104bcd70ea1d02548a40
Avi Mukesh <avimukesh10@gmail.com>
2024-12-17 10:32:11 +0000

    can drag emoji around

--- components/EmojiSticker.tsx
+++ components/EmojiSticker.tsx
@@ -16,6 +16,8 @@ type Props = {
 
 export default function EmojiSticker({ imageSize, stickerSource }: Props) {
   const scaleImage = useSharedValue(imageSize);
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
 
   const doubleTap = Gesture.Tap()
     .numberOfTaps(2)
@@ -27,20 +29,39 @@ export default function EmojiSticker({ imageSize, stickerSource }: Props) {
       }
     });
 
+  // similar to StyleSheet.create(), but this is animated
   const imageStyle = useAnimatedStyle(() => ({
     width: withSpring(scaleImage.value),
     height: withSpring(scaleImage.value),
   }));
 
+  const drag = Gesture.Pan().onChange((event) => {
+    translateX.value += event.changeX;
+    translateY.value += event.changeY;
+  });
+
+  const containerStyle = useAnimatedStyle(() => ({
+    transform: [
+      {
+        translateX: translateX.value,
+      },
+      {
+        translateY: translateY.value,
+      },
+    ],
+  }));
+
   return (
-    <View style={{ top: -350 }}>
-      <GestureDetector gesture={doubleTap}>
-        <Animated.Image
-          source={stickerSource}
-          resizeMode="contain"
-          style={[imageStyle, { width: imageSize, height: imageSize }]}
-        />
-      </GestureDetector>
-    </View>
+    <GestureDetector gesture={drag}>
+      <Animated.View style={[containerStyle, { top: -350 }]}>
+        <GestureDetector gesture={doubleTap}>
+          <Animated.Image
+            source={stickerSource}
+            // resizeMode="contain"
+            style={[imageStyle, { width: imageSize, height: imageSize }]}
+          />
+        </GestureDetector>
+      </Animated.View>
+    </GestureDetector>
   );
 }
